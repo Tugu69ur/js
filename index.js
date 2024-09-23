2fcff8d1d7f48d9f36c517c06939983523b7109c
Tugu69ur <149983686+Tugu69ur@users.noreply.github.com>
2024-09-23 09:25:39 +0800

    tip

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-console.log("Hi huurhnu")
-console.log("Hi huurhnu")
-console.log("Hi huurhnu");
+const percentage = (tip, value) => {
+  let per = (value / tip);
+  return per;
+};
+
+const NiitDun = (tip, value) => {
+  let sum = tip + value;
+  return sum;
+};
+
+let value = Number(prompt("Cost?"));
+
+let tip = Number(prompt("Tip?"));
+
+alert('Total amount:'+NiitDun(tip, value));
+alert("Tip:" + percentage(tip, value)+"%");
